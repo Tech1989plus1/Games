f18ac9b5ca0afedd3a403b4b9c85f900ea655f76
DNM <DNM@derricks-mbp>
2018-01-26 10:36:35 -0800

    update

--- Project2/src/com/View/viewInterface.java
+++ Project2/src/com/View/viewInterface.java
@@ -2,22 +2,32 @@ package com.View;
 
 public interface viewInterface{
 
+//start() method is a UI and should be the starting point of the class
+// the interface ask the user to make a section on following input(int) values
+// returns input(int)
 public int start();
 
+//setRow() gets user input(int)
 public void setRow();
 
+//getRow() returns row(int) 
 public int getRow();
 
+//setColumn() gets user input(int)
 public void setColumn();
 
+//getColumn() returns column(int) 
 public int getColumn();
 
-//Move method user enters in a row and column; returns nothing
+//move() method allows the user to make a move to the board
+// return nothing. It updates board in this method
 public void move();
 
-//checkValid method checks to see if user input is correct
-//returns input value, passes in a min in max value
+//checkValid() method checks to see if user input is correct
+//parameters min and max value, returns valid input
 public int checkValid(int min, int max);
 
+//printBoard() method prints the board, and does not passes any parameters
+// no return
 public void printBoard();
 }
