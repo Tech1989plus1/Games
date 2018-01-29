eea284dc1f3137223e2524aa53879022f9a25942
DNM <DNM@derricks-mbp>
2018-01-29 10:39:09 -0800

    viewUnitTesting

---
+++ Project2/src/com/View/viewUnitTest.java
@@ -0,0 +1,35 @@
+package com.View;
+
+public class viewUnitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		userMove nonoMoves = new userMove();
+		viewNonogram nono = new viewNonogram();
+		
+		nono.start();
+		System.out.println("Column " + nono.getColumn());
+		System.out.println("Row: " + nono.getRow());
+		
+		nonoMoves = nono.move();
+		
+		System.out.println("Move method row number: " + nonoMoves.getRow());
+		System.out.println("Move method column number: " + nonoMoves.getColumn());
+		
+		userMove sudoMoves = new userMove();
+		viewSudoku suko = new viewSudoku();
+		
+		suko.start();
+		System.out.println("Column " + suko.getColumn());
+		System.out.println("Row: " + suko.getRow());
+		
+		sudoMoves = suko.move();
+		
+		System.out.println("Move method row number: " + sudoMoves.getRow());
+		System.out.println("Move method column number: " + sudoMoves.getColumn());
+		System.out.println("Move method input value: " + sudoMoves.getInput());
+		
+	}
+
+}
